7b0c5475ed0d580ee4dd72e6761d96b48d966437
Naren Dasan <naren@narendasan.com>
2022-06-22 15:06:13 -0700

    Add noisy and adversarial user models to the user simulator

--- src/simulate_users.py
+++ src/simulate_users.py
@@ -44,11 +44,19 @@ weights = [[0.60, 0.20, 0.20],
            [0.30, 0.20, 0.50],
            [0.20, 0.30, 0.50]]
 
-adversarial_weights = np.array(weights)
-adversarial_weights[:, [0, 1, 2]] = adversarial_weights[:, [2, 0, 1]]
+noisy_weights = np.array(weights)
+noisy_weights += np.random.normal(0, 1, noisy_weights.shape)
+
+adversarial_weights = 1 - np.array(weights)
+
+print(f"Weights: {weights}")
+print(f"Noisy Weights: {noisy_weights}")
+print(f"Adversarial Weights: {adversarial_weights}")
+
 
 canonical_actions = list(range(len(canonical_features)))
 complex_actions = list(range(len(complex_features)))
+
 
 # initialize canonical task
 C = CanonicalTask(canonical_features)
@@ -65,7 +73,7 @@ X.set_terminal_idx()
 all_complex_trajectories = X.enumerate_trajectories([complex_actions])
 
 # loop over all users
-canonical_demos, complex_demos = [], []
+canonical_demos, complex_demos, noisy_demos, adversarial_demos = [], [], [], []
 for i in range(len(weights)):
 
     print("=======================")
@@ -79,21 +87,39 @@ for i in range(len(weights)):
 
     canonical_rewards = canonical_abstract_features.dot(weights[i])
     complex_rewards = complex_abstract_features.dot(weights[i])
+    noisy_rewards = complex_abstract_features.dot(noisy_weights[i])
+    adversarial_rewards = complex_abstract_features.dot(adversarial_weights[i])
+
 
     qf_canonical, _, _ = value_iteration(C.states, C.actions, C.transition, canonical_rewards, C.terminal_idx)
     qf_complex, _, _ = value_iteration(X.states, X.actions, X.transition, complex_rewards, X.terminal_idx)
+    qf_noisy, _, _ = value_iteration(X.states, X.actions, X.transition, noisy_rewards, X.terminal_idx)
+    qf_adversarial, _, _ = value_iteration(X.states, X.actions, X.transition, adversarial_rewards, X.terminal_idx)
 
     canonical_demo = rollout_trajectory(qf_canonical, C.states, C.transition, canonical_actions)
     complex_demo = rollout_trajectory(qf_complex, X.states, X.transition, complex_actions)
+    noisy_demo = rollout_trajectory(qf_noisy, X.states, X.transition, complex_actions)
+    adversarial_demo = rollout_trajectory(qf_adversarial, X.states, X.transition, complex_actions)
+
 
     canonical_demos.append(canonical_demo)
     complex_demos.append(complex_demo)
-    print("Canonical demo:", canonical_demo)
-    print("  Complex demo:", complex_demo)
+    noisy_demos.append(noisy_demo)
+    adversarial_demos.append(adversarial_demo)
+
+    print("  Canonical demo:", canonical_demo)
+    print("    Complex demo:", complex_demo)
+    print("      Noisy demo:", noisy_demo)
+    print("Adversarial demo:", noisy_demo)
+
 
 np.savetxt("data/user_demos/weights.csv", weights)
+np.savetxt("data/user_demos/noisy_weights.csv", noisy_weights)
+np.savetxt("data/user_demos/adversarial_weights.csv", adversarial_weights)
 np.savetxt("data/user_demos/canonical_demos.csv", canonical_demos)
 np.savetxt("data/user_demos/complex_demos.csv", complex_demos)
+np.savetxt("data/user_demos/noisy_demos.csv", noisy_demos)
+np.savetxt("data/user_demos/adversarial_demos.csv",  adversarial_demos)
 pickle.dump(all_canonical_trajectories, open("data/user_demos/canonical_trajectories.csv", "wb"))
 pickle.dump(all_complex_trajectories, open("data/user_demos/complex_trajectories.csv", "wb"))
 
