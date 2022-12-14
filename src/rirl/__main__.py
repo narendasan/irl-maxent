fccda47285e08cd57563d1928e4374427c82eeb4
Naren Dasan <naren@narendasan.com>
2022-12-14 09:38:52 -0800

    new metrics

--- src/rirl/__main__.py
+++ src/rirl/__main__.py
@@ -89,6 +89,8 @@ def unique_cumulative_features_metric(experiments: Dict[int, List[TrajectoryResu
         task_scores[i] = unique_cumulative_feats.shape[0] / len(cumulative_feats)
     return task_scores
 
+#TODO: Implement a dispersion metric that uses cosine similarity instead of distance. See notes for details
+#TODO: Figure out why the plot shows some particularly bad tasks for the best and good for the worst.
 def dispersion_metric(experiments: Dict[int, List[TrajectoryResult]]) -> Dict[int, float]:
     def F(trajectory_i: TrajectoryResult):
         return trajectory_i.cumulative_seen_features / len(trajectory_i.trajectory)
@@ -104,12 +106,59 @@ def dispersion_metric(experiments: Dict[int, List[TrajectoryResult]]) -> Dict[in
         task_scores[i] = np.sum(dispersion_inner) / (len(trajectories) - 1)
     return task_scores
 
+def normed_dispersion_metric(experiments: Dict[int, List[TrajectoryResult]]) -> Dict[int, float]:
+    def F(trajectory_i: TrajectoryResult):
+        return trajectory_i.cumulative_seen_features / len(trajectory_i.trajectory)
+
+    def H(f_trajectory_i: np.array):
+        f_trajectory_i_hat = f_trajectory_i / np.linalg.norm(f_trajectory_i)
+        return f_trajectory_i_hat
+
+    task_scores = {}
+    for i, trajectories in experiments.items():
+        F_is = np.vstack([H(F(t)) for t in trajectories])
+        F_bar = np.mean(F_is, axis=0)
+        tiled_F_bar = np.tile(F_bar, (len(trajectories), 1))
+        diff = F_is - tiled_F_bar
+        dispersion_inner = np.einsum("ij,ij -> i", diff, diff)
+        assert(dispersion_inner.shape == (len(trajectories),))
+        task_scores[i] = np.sum(dispersion_inner) / (len(trajectories) - 1)
+    return task_scores
+
+def cos_dispersion_metric(experiments: Dict[int, List[TrajectoryResult]]) -> Dict[int, float]:
+    def F(trajectory_i: TrajectoryResult):
+        return trajectory_i.cumulative_seen_features / len(trajectory_i.trajectory)
+
+    def H(f_trajectory_i: np.array):
+        f_trajectory_i_hat = f_trajectory_i / np.linalg.norm(f_trajectory_i)
+        assert(f_trajectory_i_hat.shape[-1] == 3)
+        hxy = np.hypot(f_trajectory_i_hat[0], f_trajectory_i_hat[1])
+        r = np.hypot(hxy, f_trajectory_i_hat[2])
+        el = np.arctan2(f_trajectory_i_hat[2], hxy)
+        az = np.arctan2(f_trajectory_i_hat[1], f_trajectory_i_hat[0])
+        #only angles?
+        return np.hstack((el, az))
+
+    task_scores = {}
+    for i, trajectories in experiments.items():
+        F_is = np.vstack([H(F(t)) for t in trajectories])
+        F_bar = np.mean(F_is, axis=0)
+        tiled_F_bar = np.tile(F_bar, (len(trajectories), 1))
+        diff = F_is - tiled_F_bar
+        dispersion_inner = np.einsum("ij,ij -> i", diff, diff)
+        assert(dispersion_inner.shape == (len(trajectories),))
+        task_scores[i] = np.sum(dispersion_inner) / (len(trajectories) - 1)
+    return task_scores
+
+
 Metric = namedtuple("Metric", ["name", "func"])
 
 METRICS = {
     "unique-trajectories": Metric("unique trajectories / sampled weights", unique_trajectories_metric),
     "unique-cumulative-features": Metric("unique cumulative features / sampled weights", unique_cumulative_features_metric),
     "dispersion": Metric("dispersion", dispersion_metric),
+    "normed-dispersion": Metric("normed-dispersion", cos_dispersion_metric),
+    "cos-dispersion": Metric("cos-dispersion", cos_dispersion_metric),
 }
 
 def task_feat_subset(task_feats: Dict[int, np.array], task_ids: List[int]) -> List[np.array]:
@@ -125,6 +174,8 @@ def main():
         raise RuntimeError(f"Invalid metric {args.metric} (valid metrics: {list(METRICS.keys())})")
     # Sample at the start a bunch of agent weights (~1000) [1xnum_feats]
     # TODO: Look at other sampling methods to more effectively cover the trajectory space.
+    # TODO: 12/14: Sample from the surface of a unit hyper-sphere
+    # TODO: 12/14: Use DDP to sample them
     agent_feature_weights = np.random.normal(loc=0.0, scale=1.0, size=(args.weight_samples, args.feature_space_size))
 
     cluster = LocalCluster(
@@ -204,14 +255,7 @@ def main():
             best_task_feats_by_action_space[action_space_size] = [task_feats[k] for k in best_tasks]
             worst_task_feats_by_action_space[action_space_size] = [task_feats[k] for k in worst_tasks]
 
-
-            # TODO: For the best and worst tasks, run IRL on the cannonical task trajectory to estimate the weights of an agent,
-            # Then sample an "actual task" (double the action space size), and compare the trajectory estimated to the g.t trajectory
-            # Use predict_trajectory from the other code, (one action at a time, if action is right +1 else 0, then take g.t action and repeat.)
-
-            # TODO: [DO FIRST] Plot action space size vs. number of unqiue trajectories / num sampled agents for best and worst tasks.
-
-        # TODO: Plot action space size vs. number of unique feature sums / num sampled agents for best and worst tasks.
+        # TODO: Save the actual metric numbers to a table
 
         print(f"Action space vs. Number of unqiue trajectories for {args.weight_samples} sampled agents based on {METRICS[args.metric].name}: {best_score_by_action_space}")
         print(f"Action space vs. Number of unqiue trajectories for {args.weight_samples} sampled agents based on {METRICS[args.metric].name}: {worst_score_by_action_space}")
